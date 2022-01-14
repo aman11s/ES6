e0a0c68c5197a3d8d5e2cde8211bab6c44798184
Aman Singh <reachout.amansingh@gmail.com>
2022-01-14 17:13:43 +0530

    feat : added que 2

--- ES6+ Assignment/index.js
+++ ES6+ Assignment/index.js
@@ -10,9 +10,17 @@
  console.log(aloo); */
 
 let aloo = 1;
-if(aloo === 1) {
+if (aloo === 1) {
     let a = 2;
     console.log(a);
 }
 console.log(aloo)
 
+/* Que 2
+
+var multiply = function(x, y) {
+  return x * y;
+}; */
+
+const multiply = (x, y) => x * y;
+console.log(multiply(2, 3));
