d2db6da471da51bcf2f39d8662fe25738593af47
Aman Singh <reachout.amansingh@gmail.com>
2022-01-14 23:39:47 +0530

    feat : added que 7, 8, 9

--- ES6+ Assignment/index.js
+++ ES6+ Assignment/index.js
@@ -37,7 +37,9 @@ var card = {
 };
 var message = "Hello " + customer.name + " wants to buy " + card.amount + " " + card.product + " for price of " + card.unitprice + " per piece" */
 
-const customer = { name: 'Bhaalo' };
+const customer = {
+    name: 'Bhaalo'
+};
 const card = {
     amount: 20,
     product: 'Aaalo',
@@ -81,7 +83,10 @@ var Obj = {
 
 const Aaloo = 'Tasty';
 const Bhaalo = 'Cute';
-const Obj = { Aaloo, Bhaalo };
+const Obj = {
+    Aaloo,
+    Bhaalo
+};
 console.log(Obj);
 
 /* Que 7
@@ -90,5 +95,62 @@ var a = 5;
 var b = 10;
 console.log("Fifteen is ".concat(a + b, " and n0t ").concat(2 * a + b, ".")); */
 
-const a = 5, b = 10;
+const a = 5,
+    b = 10;
 console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`)
+
+/* Que 8
+
+var arithmeticsObj = {
+  sum: function sum(num1, num2) {
+    return num1 + num2;
+  },
+  multiply: function multiply(num1, num2) {
+    return num1 * num2;
+  }
+}; */
+
+const arithmeticsObj = {
+    sum(num1, num2) {
+        return num1 + num2;
+    },
+    multiply(num1, num2) {
+        return num1 * num2;
+    }
+};
+
+console.log(arithmeticsObj.sum(2, 4));
+console.log(arithmeticsObj.multiply(2, 10));
+
+/* Que 9
+
+var avengers = {
+  operation: "Assemble",
+  members: [
+    {
+      ironMan: "Tony Stark"
+    },
+    {
+      captainAmerica: "Steve Rogers"
+    },
+    {
+      blackWidow: "Natasha Romanoff"
+    }
+  ]
+};
+var operation = avengers.operation,
+  members = avengers.members; */
+
+  const avengers = {
+      operation : 'Assemble',
+      members: [
+          { ironMan: 'Tony Stark' },
+          { captainAmerica: 'Steve Rogers' },
+          { blackWidow: 'Natasha Romanoff' }
+      ]
+  };
+
+  const {operation, members} = avengers;
+
+  console.log(operation);
+  console.log(members);
