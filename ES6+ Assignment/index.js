1e26c9db109aa48aa4c0484cae01ed21dd56068a
Aman Singh <reachout.amansingh@gmail.com>
2022-01-14 17:11:08 +0530

    feat : added example 1

---
+++ ES6+ Assignment/index.js
@@ -0,0 +1,18 @@
+// Covert code to ES6+ syntax
+
+/* Que 1 
+
+ var aloo = 1;
+ if (aloo == 1) {
+    var a = 2;
+    console.log(a);
+ }
+ console.log(aloo); */
+
+let aloo = 1;
+if(aloo === 1) {
+    let a = 2;
+    console.log(a);
+}
+console.log(aloo)
+
