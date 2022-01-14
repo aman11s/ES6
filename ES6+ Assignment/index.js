e2a59b82333d4fa96b9e2913a70db4c9d2bda0fd
Aman Singh <reachout.amansingh@gmail.com>
2022-01-14 19:37:26 +0530

    feat : add que 7

--- ES6+ Assignment/index.js
+++ ES6+ Assignment/index.js
@@ -83,3 +83,12 @@ const Aaloo = 'Tasty';
 const Bhaalo = 'Cute';
 const Obj = { Aaloo, Bhaalo };
 console.log(Obj);
+
+/* Que 7
+
+var a = 5;
+var b = 10;
+console.log("Fifteen is ".concat(a + b, " and n0t ").concat(2 * a + b, ".")); */
+
+const a = 5, b = 10;
+console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`)
